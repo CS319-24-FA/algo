e1c036ad3f963450fac8f262fac3668fe4018e11
adenizgelir0 <53797734+adenizgelir0@users.noreply.github.com>
2024-12-04 00:27:09 +0300

    Update algo_ip.py
    some bug fixes

--- algo_ip.py
+++ algo_ip.py
@@ -10,7 +10,7 @@ class AlgorithmIP:
         self.guide_count = 0
         self.weekly_workload = weekly_workload
         self.max_slot_load = max_slot_load
-        self.problem = LpProblem("Scheduler", pl.LpMaximize)
+        self.problem = pl.LpProblem("Scheduler", pl.LpMaximize)
 
     def add_tour(self, id, guide_count, priority, range, fixed_pos=None, fixed=False):
         self.tours.append({"id":id, "guide_count":guide_count, "priority":priority,
@@ -34,20 +34,20 @@ class AlgorithmIP:
                          "fixed_tour_ids":fixed_tour_ids})
         self.guide_count += 1
 
-    def create_variables():
-        self.guide_labels = range(guide_count)
-        self.tour_labels = range(tours_count)
-        self.slot_labels = range(day_count*4)
-        self.assign_vars = pulp.LpVariable.dicts("assign", (guide_labels,tour_labels,slot_labels), cat="Binary")
-        self.tour_vars = pulp.LpVariable.dicts("tours", (tour_labels,slot_labels), cat="Binary")
+    def create_variables(self):
+        self.guide_labels = range(self.guide_count)
+        self.tour_labels = range(self.tour_count)
+        self.slot_labels = range(self.day_count*4)
+        self.assign_vars = pl.LpVariable.dicts("assign", (self.guide_labels,self.tour_labels,self.slot_labels), cat="Binary")
+        self.tour_vars = pl.LpVariable.dicts("tours", (self.tour_labels,self.slot_labels), cat="Binary")
 
         #forces tours to be assigned to unique time slots
         for tour_i in self.tour_labels:
             for slot_i in self.tour_labels:
-                self.problem += lpSum(self.assign_vars[guide_i][tour_i][slot_i]\
-                     for guide_i in guide_labels) == 5 * self.tour_vars[tour_i][slot_i]
+                self.problem += pl.lpSum(self.assign_vars[guide_i][tour_i][slot_i]\
+                     for guide_i in self.guide_labels) == 5 * self.tour_vars[tour_i][slot_i]
 
-    def add_fixed_constraints():
+    def add_fixed_constraints(self):
         #adds manually made adjustments as constraints
         for tour_i in self.tour_labels:
             if self.tours[tour_i]["fixed"]:
@@ -69,14 +69,14 @@ class AlgorithmIP:
                         pl.lpSum(self.assign_vars[guide_i][tour_index][slot_i]\
                              for slot_i in self.slot_labels) == 1
     
-    def add_gcount_constraints():
+    def add_gcount_constraints(self):
         #every tour must be assigned as many guides as it requires
         for tour_i in self.tour_labels:
             self.problem += pl.lpSum(self.assign_vars[guide_i][tour_i][slot_i]\
                     for guide_i in self.guide_labels \
                         for slot_i in self.slot_labels) == self.tours[tour_i]["guide_count"]
     
-    def add_availability_constraints():
+    def add_availability_constraints(self):
         #adds availability constraints
         for guide_i in self.guide_labels:
             nonavailability = []
@@ -85,55 +85,73 @@ class AlgorithmIP:
                     nonavailability.append(slot_i)
             for tour_i in self.tour_labels:
                 for slot_i in nonavailability:
-                    problem += self.assign_vars[guide_i][tour_i][slot_i] == 0
+                    self.problem += self.assign_vars[guide_i][tour_i][slot_i] == 0
     
-    def add_slotload_constraints():
+    def add_slotload_constraints(self):
         #only a certain amount of tours can start in the same tour
         for slot_i in self.slot_labels:
             self.problem += \
                 pl.lpSum(self.assign_vars[guide_i][tour_i][slot_i] \
-                    for slot_i in self.slot_labels for guide_i in self.guide_labels) \
+                    for tour_i in self.tour_labels for guide_i in self.guide_labels) \
                 <= self.max_slot_load
     
-    def add_workload_constraints():
+    def add_workload_constraints(self):
         #a guide can only work a certain amount of times per week
         for guide_i in self.guide_labels:
             for week_start in range(0, self.day_count*4, 7*4):
-                week_range = range(week_start,week_start+7*24)
+                week_range = range(week_start,min(week_start+7*4,self.day_count*4))
                 self.problem += \
                     pl.lpSum(self.assign_vars[guide_i][tour_i][slot_i] \
                         for tour_i in self.tour_labels for slot_i in week_range) \
                     <= self.weekly_workload
     
-    def add_assignonce_constraints():
+    def add_assignonce_constraints(self):
         #a guide can not be assigned to multiple tours in a single time slot
         for guide_i in self.guide_labels:
             for slot_i in self.slot_labels:
-                self.problem += pl.lpSum(self.assign_vars[guide_i][tour_i][slot_1] \
+                self.problem += pl.lpSum(self.assign_vars[guide_i][tour_i][slot_i] \
                     for tour_i in self.tour_labels) <= 1
+    
+    def add_range_constraints(self):
+        for tour_i in self.tour_labels:
+            for slot_i in self.slot_labels:
+                low = self.tours[tour_i]["range"][0]
+                high = self.tours[tour_i]["range"][1]
+                if low > slot_i or slot_i > high:
+                    self.problem += pl.lpSum(self.assign_vars[guide_i][tour_i][slot_i] \
+                        for guide_i in self.guide_labels) == 0
 
-    def initialize():
+    def initialize(self):
         #adds all the constraints
         self.create_variables()
+        print("created_variables")
         self.add_fixed_constraints()
+        print("added fixed constraints")
         self.add_gcount_constraints()
+        print("added gcount constraints")
         self.add_availability_constraints()
+        print("added availability constraints")
         self.add_slotload_constraints()
+        print("added slotload constraints")
         self.add_workload_constraints()
+        print("added workload constraints")
         self.add_assignonce_constraints()
+        print("added assignonce constraints")
+        self.add_range_constraints()
+        print("added range constraints")
 
-        problem += lpSum(self.assign_vars[guide_i][tour_i][slot_i] * self.guides[guide_i]["rating"] * self.tours[tour_i]["priority"]\
-                    for guide_i in self.guide_labels for tours_i in self.tour_labels\
+        self.problem += pl.lpSum(self.assign_vars[guide_i][tour_i][slot_i] * self.guides[guide_i]["rating"] * self.tours[tour_i]["priority"]\
+                    for guide_i in self.guide_labels for tour_i in self.tour_labels\
                          for slot_i in self.slot_labels), "Objective"
 
-    def solve():
-        self.problem.solve()
+    def solve(self):
+        self.problem.solve(pl.PULP_CBC_CMD(gapRel = 0.1))
         guide_ret = {}
         tours_ret = {}
         for guide_i in self.guide_labels:
             for tour_i in self.tour_labels:
                 for slot_i in self.slot_labels:
-                    var_value = value(self.assign_vars[guide_i][tour_i][slot_i])
+                    var_value = pl.value(self.assign_vars[guide_i][tour_i][slot_i])
                     if var_value == 0:
                         continue
                     guide_id = self.guides[guide_i]["id"]
@@ -143,3 +161,4 @@ class AlgorithmIP:
                     guide_ret[guide_id].append(tour_id)
                     tours_ret[tour_id] = slot_i
         return guide_ret, tours_ret
+
